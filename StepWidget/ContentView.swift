332e857d19c381093ef8e87b4523beae8988728a
div-antk <51283468+owaii@users.noreply.github.com>
2023-02-21 17:51:54 +0900

    Initial Commit

---
+++ StepWidget/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StepWidget
+//
+//  Created by Takuya Ando on 2023/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StepWidget/StepWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepWidgetApp.swift
+//  StepWidget
+//
+//  Created by Takuya Ando on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct StepWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
