f2da818207c58bf4feeb0a80127e2a5070c93425
div-antk <51283468+owaii@users.noreply.github.com>
2023-02-23 23:59:00 +0900

    歩行中かどうかの判定処理の追加

--- StepWidget/MyPedometer.swift
+++ StepWidget/MyPedometer.swift
@@ -10,6 +10,33 @@ import CoreMotion
 
 class MyPedometer: NSObject, ObservableObject {
     
+    @Published var isStarted = false
+    @Published var isWalking = false
+    @Published var count = 0
+    
     let pedometer = CMPedometer()
     
+    func start() {
+        guard !isStarted else {
+            return
+        }
+        
+        isStarted = true
+        
+        pedometer.stopEventUpdates { (event, error) in
+            guard error == nil else {
+                print("error: \(String(describing: error))")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                if event!.type == CMPedometerEventType.pause {
+                    self.isWalking = false
+                } else {
+                    self.isWalking = true
+                }
+            }
+        }
+    }
 }
+
